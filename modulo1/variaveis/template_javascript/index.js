7ea2d056f01f78b32d3da9dff3b263d96762144d
Edson Schiffer <schiffer1212@gmail.com>
2022-02-10 11:31:45 -0300

    Apenas pequenas atualizações

--- modulo1/variaveis/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/index.js
@@ -4,9 +4,29 @@
 
 /* 1 - Foi atribuído o valor 10 para as variáveis 'a' e 'b', mas foi apenas imprimida a 'b'. mais abaixo o valor atribuído ao 'b' passou a ser 5 e após isso foi imprimido o valor de 'a' e 'b' que agora será 10 para 'a' e 5 para 'b' */
 
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
 /* 2 - Foi atribuído o valor 10 para a variável 'a' e 20 para a 'b' após isso o valor de 'c' virou 'a', 'b' virou 'c' e o valor de 'a' virou o de 'b'. ou seja, todos os valores das variáveis passaram a ser de 10 */
 
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
 /* 3 - Aqui aparecerá 2 pop-ups com caixa de texto e um alerta, o primeiro pop-up irá perguntar quantas horas eu trabalho por dia, o segundo irá perguntar quanto recebo por dia e no final, o alerta irá me informar quanto recebo por hora */
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
 
 
                     // Exercícios de escrita de código
