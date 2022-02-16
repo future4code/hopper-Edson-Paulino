e65a395e28e535c2cf1f5c6056ef0595aa17d8fb
Edson Schiffer <schiffer1212@gmail.com>
2022-02-15 21:39:30 -0300

    Exercicio strings arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,62 @@
+//                                            Exercicio de interpretação
+
+// 1 -
+
+// A - Retorna "Undefined" como valor da variável.
+
+// B - Retorna "Null" como valor da variável.
+
+// C - Retorna a quantidade de itens dentro da arrya(11)
+
+// D - Retorna o índice incial que é o número 3.
+
+// E - Retorna a adição do numero 19 no índice 1.
+
+// F - Retorna a soma do índice inicial mais 6 que resulta em 9.
+
+// 2 - 
+
+// Será trocado todos "A" por "I" e o tamanho da variável será 27.
+
+
+//                                      Exercicio de escrita de código
+
+// 1 -
+
+/*
+let nomeDoUsuario = prompt("Digite seu nome: ")
+let emailDoUsuario = prompt("Digite seu email: ")
+
+console.log(`O email ${emailDoUsuario} foi cadastrado com sucesso. seja bem-vindo(a), ${nomeDoUsuario}!`)
+*/
+
+/* let comidasPreferidas = ["Lasanha", "Pizza", "Sushi", "Escondidinho de carne", "Hamburguer"]
+console.log(comidasPreferidas)
+console.log(`Essas são as minhas comidas preferidas: 
+${comidasPreferidas [0]}
+${comidasPreferidas [1]}
+${comidasPreferidas [2]}
+${comidasPreferidas [3]}
+${comidasPreferidas [4]}`) 
+*/
+
+
+/*let listaDeTarefas = []
+let tarefas1 = listaDeTarefas.push(prompt("Tarefa 1:"))
+let tarefas2 = listaDeTarefas.push(prompt("Tarefa 2:"))
+let tarefas3 = listaDeTarefas.push(prompt("Tarefa 3:"))
+let tarefaRealizada = listaDeTarefas.splice(prompt("Digite a tarefa índice concluída:"), 1)
+
+console.log(listaDeTarefas)*/
+
+
+
+
+
+
+
+
+
+
+
+
