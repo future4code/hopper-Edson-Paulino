45747462bea70374dbe376d4a8f8c1462f44dd83
Edson Schiffer <schiffer1212@gmail.com>
2022-02-17 20:00:43 -0300

    exercício de função

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,111 @@
+//                                   EXERCÍCIO DE INTEPRETAÇÃO
+
+// 1 -
+
+// A - Irá ser impresso a multiplicação de 2 x 5 que resulta em 10.
+// B - Não apareceria nada no console.
+
+// 2 - 
+
+// A - A função transforma todo o texto do usuário em minúsculo e depois procura por "cenoura" dentro do texto.
+// B - I. TRUE II. TRUE III. TRUE
+
+
+//                                   EXERCÍCIO DE ESCRITA DE CÓDIGO
+
+
+// 1 -  
+
+//     let nome = "Edson"
+//     let idade = 22
+//     let cidade = "Penedo"
+//     let formadoOuEstudante = "Estudante"
+ 
+
+//  function imprimirMensagem () {
+
+//      console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${formadoOuEstudante}`)
+//  }
+
+//  imprimirMensagem()
+
+// 2 - 
+
+// A -
+
+// function SomaNumeros (num1, num2) {
+//     let soma = num1 + num2
+//     return soma 
+// }
+
+// console.log(SomaNumeros(5,10))
+
+// B - 
+
+// function compararNumeros (numero1, numero2) {
+//     let comparar = numero1 >= numero2
+//     return comparar 
+// }
+
+// console.log(compararNumeros(8, 10))
+
+// C - 
+
+// function imparPar (numero) {
+
+//     return (numero % 2) === 0
+// }
+
+// console.log(imparPar(2))
+
+// D - 
+
+// function escreverMensagem (mensagem) {
+//     console.log(mensagem.length, mensagem.toUpperCase())
+// }
+
+// escreverMensagem("Eu sou o Edson")
+
+
+// 3 ----
+
+// SOMA
+
+//  let num1 = Number(prompt("Insira um número:"))
+//  let num2 = Number(prompt("Insira outro número:"))
+
+// function soma (num1, num2) {
+//     let somaDoisNumeros = num1 + num2
+//     return somaDoisNumeros
+//  }
+
+// console.log(soma(num1, num2))    
+
+// DIFERENÇA
+
+// function diferença (num1, num2) {
+//     let diferencaDoisNumeros = num1 - num2
+//      return diferencaDoisNumeros
+//  }
+
+//  console.log(diferença(num1, num2))
+
+// MULTIPLICAÇÃO
+
+// function multiplicacao (num1, num2) {
+//     let multiplicacaoDoisNumeros = num1 * num2
+//     return multiplicacaoDoisNumeros
+// }
+
+// console.log(multiplicacao(num1, num2))
+
+// DIVISÃO 
+
+// function divisao (num1, num2) {
+//     let divisaoDoisNumeros = num1 / num2
+//     return divisaoDoisNumeros
+// }
+
+// console.log(divisao(num1, num2))
+
+
