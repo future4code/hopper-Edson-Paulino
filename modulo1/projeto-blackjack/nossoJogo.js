cd9f966a85feb0f1d4ccbf563e68a11ddc23d5a2
Edson Schiffer <schiffer1212@gmail.com>
2022-03-08 09:29:09 -0300

    Exercício blackJack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,54 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+// console.log('Bem vindos ao Jogo BlackJack');
+
+// let jogo = confirm('Quer iniciar uma nova rodada?')
+
+// if (jogo === false) {
+//    console.log('O jogo acabou...')
+// } else if (jogo === true) {
+//    let carta1Usuario = comprarCarta()
+//    let carta2Usuario = comprarCarta()   
+//    let carta1Pc = comprarCarta()
+//    let carta2Pc = comprarCarta()
+
+//    let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+//    let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
+
+//    console.log(`Usuário - cartas: ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
+//    console.log(`Computador - cartas: ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
+
+//    if (pontuacaoUsuario > pontuacaoPc) {
+//       console.log('O Usuário ganhou!')
+//    } else if (pontuacaoPc > pontuacaoUsuario) {
+//       console.log('O Computador ganhou!')
+//    } else {
+//       console.log('Empate!')
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
